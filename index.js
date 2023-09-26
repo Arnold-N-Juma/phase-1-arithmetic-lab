1bf4bc7e7f86f6893076f9eb79263c4d55fa1fab
Arnold Juma <+>
2023-09-26 11:15:00 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-
 // Write your code here
+// Define the multiply function
+    const num1=62;
+    const num2=1;
+    const multiply=(num1*num2);
+    console.log(multiply)
+  
+     const random =5;
+     const maths=Math.floor(math.random *10)+1;
+     console.log(maths);
+      
+   
+     const num3 = 15;
+     const num4 = 11;  
+
+
+    const mod = num3 % num4
+
+
+    if (mod === 4) {
+    console.log("The remainder is 4.");} 
+    else {
+    console.log("The remainder is not 4.");
+    } 
+   
+    function max() {
+        
+        const numbers = [10, 5, 20, 15, 8];
+        const max = Math.max(... numbers);
+        console.log(max)
+    
+    }
+     
